2bdb60f9a2335409d479777bcff80242c893a15d
Jorge David <484084+jorda0mega@users.noreply.github.com>
2021-03-26 00:49:28 -0400

    fix: change tab selection upon click

--- src/main/react_state_examples/views.cljs
+++ src/main/react_state_examples/views.cljs
@@ -5,15 +5,15 @@
 
 (rf/reg-event-db
   :select-tab
-  (fn [db [_ new-selected-tab]]))
-;(let [tabs (:tabs db)]
-;  (map (fn [idx] (update-in idx [:selected (=)]))))
-;(assoc db :selected-tab new-selected-tab)))
+  (fn [db [_ new-selected-tab]]
+    (.log js/console (get-in db [:tabs]))
+    (update-in db [:tabs] #(map (fn [tab]
+                                  (assoc tab :selected (= (:id tab) new-selected-tab))) %))))
 
 (defn change-selected-tab
   "changes the selected attribute of a tab to true"
-  [db tab-id]
-  (update-in db [:tabs tab-id :selected] (identity true)))
+  [tab tab-id]
+  (assoc tab :selected (= (:id tab) tab-id)))
 
 (defn dispatch-select-tab-event
   [new-selected-tab]
@@ -32,15 +32,16 @@
         label (:label tab)
         selected? (:selected tab)]
     (if selected?
-      [:a.bg-gray-100.text-gray-700.px-3.py-2.font-medium.text-sm.rounded-md {:key      idx
-                                                                              :href     "#" :aria-current "page"
-                                                                              :on-click #(dispatch-select-tab-event id)}
+      [:a.bg-gray-100.text-gray-700.px-3.py-2.font-medium.text-sm.rounded-md
+       {:key      idx
+        :href     "#" :aria-current "page"
+        :on-click #(dispatch-select-tab-event id)}
        label]
-      [:a.text-gray-500.hover:text-gray-700.px-3.py-2.font-medium.text-sm.rounded-md {:key      idx
-                                                                                      :href     "#"
-                                                                                      :on-click #(dispatch-select-tab-event id)}
+      [:a.text-gray-500.hover:text-gray-700.px-3.py-2.font-medium.text-sm.rounded-md
+       {:key      idx
+        :href     "#"
+        :on-click #(dispatch-select-tab-event id)}
        label])))
-
 
 (defn Tabs
   "Component to display tabs"
@@ -50,7 +51,3 @@
     [:nav.flex-grow.space-x-4 {:aria-label "Tabs"}
      (let [tabs @(rf/subscribe [:tabs])]
        (map-indexed Tab tabs))]]])
-;[:a.text-gray-500.hover:text-gray-700.px-3.py-2.font-medium.text-sm.rounded-md {:href "#"} "Hello React"]
-;[:a.text-gray-500.hover:text-gray-700.px-3.py-2.font-medium.text-sm.rounded-md {:href "#"} "Lorem Ipsum"]
-;[:a.bg-gray-100.text-gray-700.px-3.py-2.font-medium.text-sm.rounded-md {:href "#" :aria-current "page"} "Login Form"]
-;[:a.text-gray-500.hover:text-gray-700.px-3.py-2.font-medium.text-sm.rounded-md {:href "#"} "Fun People"]]]])

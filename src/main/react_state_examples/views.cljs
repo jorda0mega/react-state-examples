893f10d03c746d2f64c0b700885fce468b8e780d
Jorge David <484084+jorda0mega@users.noreply.github.com>
2021-03-27 01:34:19 -0400

    fix: tab size for better visibility

--- src/main/react_state_examples/views.cljs
+++ src/main/react_state_examples/views.cljs
@@ -43,12 +43,12 @@
         label (:label tab)
         selected? (:selected tab)]
     (if selected?
-      [:a.bg-gray-100.text-gray-700.px-3.py-2.font-medium.text-sm.rounded-md
+      [:a.bg-gray-100.text-gray-700.px-3.py-2.font-large.text-md.rounded-md
        {:key      idx
         :href     "#" :aria-current "page"
         :on-click #(dispatch-select-tab-event id)}
        label]
-      [:a.text-gray-500.hover:text-gray-700.px-3.py-2.font-medium.text-sm.rounded-md
+      [:a.text-gray-500.hover:text-gray-700.px-3.py-2.font-large.text-md.rounded-md
        {:key      idx
         :href     "#"
         :on-click #(dispatch-select-tab-event id)}
